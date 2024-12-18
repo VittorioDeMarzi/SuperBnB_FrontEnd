2f4bcc347917244bf237ee0107888f08c7ca14b1
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-12-18 15:53:52 +0100

    add PaymentMethod

---
+++ src/components/ProfileSection.jsx
@@ -0,0 +1,9 @@
+export default function ProfileSection() {
+    return (
+        <>
+            <h3 className=" text-center text-ocra font-bold text-3xl mb-6">
+          Profile Informations
+        </h3>
+        </>
+    )
+}
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,25 +1,14 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import BookingRequestInfo from "../components/BookingRequestInfo";
+import Payment from "../components/Payment";
+import ProfileSection from "../components/ProfileSection";
 
 export default function BookingPage() {
   const location = useLocation();
   const { formData, availability } = location.state || {};
-  const [paymentMethod, setPaymentMethod] = useState("card");
-  const navigate = useNavigate();
-  const [cardDetails, setCardDetails] = useState({
-    cardHolder: "",
-    cardNumber: "",
-    expirationDate: "",
-    cvv: "",
-  });
 
-  const handleCardDetailChange = (e) => {
-    const { name, value } = e.target;
-    setCardDetails((prevDetails) => ({
-      ...prevDetails,
-      [name]: value,
-    }));
-  };
+  const navigate = useNavigate();
 
   async function confirmBooking() {
     try {
@@ -53,114 +42,11 @@ export default function BookingPage() {
   return (
     <div className="mx-auto p-12 min-h-screen">
       <div className="p-12 bg-slate-50 grid grid-flow-row gap-5 shadow-md">
-        <h2 className=" text-center text-ocra font-bold text-5xl">
-          Payment Confirmation
-        </h2>
-        <div className="grid gap-6">
-          <p>
-            <strong>Adults:</strong> {formData.numAdults}
-          </p>
-          <p>
-            <strong>Children:</strong> {formData.numChildren}
-          </p>
-          <p>
-            <strong>Check-in:</strong> {formData.checkInDate}
-          </p>
-          <p>
-            <strong>Check-out:</strong> {formData.checkOutDate}
-          </p>
-          <p>
-            <strong>Nights:</strong> {availability.numNights}
-          </p>
-          <p>
-            <strong>Price per Night:</strong> €{availability.pricePerNight}
-          </p>
-          <p className="underline">
-            <strong>Total Price:</strong> €{availability.totalPrice}
-          </p>
-        </div>
-        <h3 className=" text-center text-ocra font-bold text-3xl mb-6">
-          Payment method
-        </h3>
-        <div className="flex gap-12 ">
-          <button
-            onClick={() => setPaymentMethod("card")}
-            className="p-4 text-lg rounded-md min-w-44"
-            style={{
-              border:
-                paymentMethod === "card"
-                  ? "2px solid #ba8a3b"
-                  : "1px solid #ccc",
-              backgroundColor: paymentMethod === "card" ? "#ffe8ca" : "#f9f9f9",
-              boxShadow:
-                paymentMethod === "card"
-                  ? "0 4px 8px rgba(0, 0, 0, 0.2)"
-                  : "none",
-              cursor: "pointer",
-            }}
-          >
-            Carta di credito
-          </button>
-          <button
-            className="p-4 text-lg rounded-md min-w-44"
-            onClick={() => setPaymentMethod("paypal")}
-            style={{
-              border:
-                paymentMethod === "paypal"
-                  ? "2px solid #ba8a3b"
-                  : "1px solid #ccc",
-              backgroundColor:
-                paymentMethod === "paypal" ? "#ffe8ca" : "#f9f9f9",
-              boxShadow:
-                paymentMethod === "paypal"
-                  ? "0 4px 8px rgba(0, 0, 0, 0.2)"
-                  : "none",
-              cursor: "pointer",
-            }}
-          >
-            PayPal
-          </button>
-        </div>
+        <ProfileSection />
 
-        {paymentMethod === "card" && (
-          <div className="mt-6 space-y-4">
-            <input
-              type="text"
-              name="cardHolder"
-              placeholder="cardHolder"
-              value={cardDetails.cardHolder}
-              onChange={handleCardDetailChange}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-ocra"
-            />
-            <input
-              type="text"
-              name="cardNumber"
-              placeholder="Card Number"
-              value={cardDetails.cardNumber}
-              onChange={handleCardDetailChange}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-ocra"
-            />
-            <div className="flex gap-4">
-              <input
-                type="text"
-                name="expirationDate"
-                placeholder="MM/YY"
-                value={cardDetails.expirationDate}
-                onChange={handleCardDetailChange}
-                className="w-1/2 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-ocra"
-              />
-              <input
-                type="text"
-                name="cvv"
-                placeholder="CVV"
-                value={cardDetails.cvv}
-                onChange={handleCardDetailChange}
-                className="w-1/2 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-ocra"
-              />
-            </div>
-          </div>
-        )}
+        <BookingRequestInfo formData={formData} availability={availability} />
 
+        <Payment />
         <button
           className="mt-8 w-full p-3 text-lg text-white bg-ocra rounded-lg shadow-md hover:bg-background focus:outline-none focus:ring-2 focus:ring-ocra"
           onClick={() => confirmBooking()}
